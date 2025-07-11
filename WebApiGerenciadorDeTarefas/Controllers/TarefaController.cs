b75dd01519e7b22dfadcee367d185a4ef7e915d4
fabiosoares-silva <fabiosores.soares@gmail.com>
2025-07-10 23:53:48 -0300

    Incluir diretório models e arquivo tarefacontroller

---
+++ WebApiGerenciadorDeTarefas/Controllers/TarefaController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApiGerenciadorDeTarefas.Models;
+
+namespace WebApiGerenciadorDeTarefas.Controllers
+{
+    [ApiController]
+    [Route("Controller")]
+    public class TarefaController : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public IActionResult ObterPorId(int id)
+        {
+            return Ok();
+        }
+
+        [HttpGet("ObterTodos")]   
+        public IActionResult ObterTodos()
+        {
+            return Ok();
+        }
+
+        [HttpGet("ObterPorTítulo")]
+        public IActionResult ObterPorTitulo(string titulo)
+        {
+            return Ok();
+        }
+
+        [HttpGet("ObterPorData")]
+        public IActionResult ObterPorData(DateTime data)
+        {
+            return Ok();
+        }
+
+        [HttpGet("ObterPorStatus")]
+        public IActionResult ObterPorStatus(EnumStatusTarefa status)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Criar(Tarefa tarefa)
+        {
+            if (tarefa.Data == DateTime.MinValue)
+                return BadRequest(new { Erro = "A data da tarefa não pode ser vazia"});
+
+            return CreatedAtAction(nameof(ObterPorId), new { id = tarefa.Id }, tarefa);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Atualizar(int id, Tarefa tarefa)
+        {
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Deletar(int id)
+        {
+            return NoContent();
+        }
+    }
+}
---
+++ WebApiGerenciadorDeTarefas/Models/EnumStatusTarefa.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApiGerenciadorDeTarefas.Models
+{
+    public enum EnumStatusTarefa
+    {
+        Pendente,
+        Finalizado
+    }
+}
