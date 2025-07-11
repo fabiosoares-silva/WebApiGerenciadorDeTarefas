52c2454f4fada942d50089353206e426f1058205
fabiosoares-silva <fabiosores.soares@gmail.com>
2025-07-11 09:37:49 -0300

    Configurar connection string Db local

--- WebApiGerenciadorDeTarefas/Program.cs
+++ WebApiGerenciadorDeTarefas/Program.cs
@@ -1,6 +1,12 @@
+using WebApiGerenciadorDeTarefas.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<OrganizadorContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoPadrao")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
