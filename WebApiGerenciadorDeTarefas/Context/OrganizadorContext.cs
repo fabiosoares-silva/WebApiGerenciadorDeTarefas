adca5e48bfefaf000b0e39c070ab9886704c76ae
fabiosoares-silva <fabiosores.soares@gmail.com>
2025-07-11 00:35:12 -0300

    Incluir diretório Context, arquivo organizadorcontext e baixar pacotes EF

---
+++ WebApiGerenciadorDeTarefas/Context/OrganizadorContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApiGerenciadorDeTarefas.Models;
+
+namespace WebApiGerenciadorDeTarefas.Context
+{
+    public class OrganizadorContext : DbContext
+    {
+        public OrganizadorContext(DbContextOptions<OrganizadorContext> options) : base(options) { }
+        public DbSet<Tarefa> Tarefas { get; set; }
+    }
+}
